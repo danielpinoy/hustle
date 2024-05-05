cbae2c130ab1008e20f2e7dd3458528806838a42
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2024-05-05 20:59:54 +0800

    Initial React project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Plans from "./components/Plans/Plans";
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Hero />
+            <Programs />
+            <Reasons />
+            <Plans />
+        </div>
+    );
 }
 
 export default App;
